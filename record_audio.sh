12bc125003b758de38d72b19d6d62378bb7ac30f
StewOnPi <s.agar16@imperial.ac.uk>
2023-05-19 18:05:51 +0100

    cleanup

--- record_audio.sh
+++ record_audio.sh
@@ -4,13 +4,11 @@ fs=44100
 s=$[$t*$fs]
 arecord -D plughw:Device,0 -f cd -t wav -s $s -r $fs -c 1 -q - | tee soundcard1.wav > /dev/null &
 arecord -D plughw:Device_1,0 -f cd -t wav -s $s -r $fs -c 1 -q - | tee soundcard2.wav > /dev/null &
-arecord -D hw:Device_2,0 -f cd -t wav -s $s -r $fs -c 1 -q - | tee soundcard3.wav > /dev/null &
-arecord -D hw:Device_3,0 -f cd -t wav -s $s -r $fs -c 1 -q - | tee soundcard4.wav > /dev/null &
-echo $(date) " RECORDING FOR " $t
+arecord -D plughw:Device_2,0 -f cd -t wav -s $s -r $fs -c 1 -q - | tee soundcard3.wav > /dev/null &
+arecord -D plughw:Device_3,0 -f cd -t wav -s $s -r $fs -c 1 -q - | tee soundcard4.wav > /dev/null &
+echo $(date) "RECORDING FOR" $t
 aplay chirp.wav
 sleep $t
-date
-echo "sending"
+echo $(date) "sending"
 python send_audio.py
-date
-echo "done send"
+echo $(date) "done send"
